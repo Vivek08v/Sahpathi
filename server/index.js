9871976209972e1624720d1a23e466003f4bd603
Vivek08v <vivekkumar08v@gmail.com>
2025-07-13 20:04:54 +0530

    backend files, server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// middlewars
+app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    })
+)
+
+// routes
+app.get("/", (req, res)=>{
+    res.json({
+        success: true,
+        message: "Your server is up and running..."
+    })
+})
+
+// listening to server
+app.listen(4000, ()=>{
+    console.log("Server started at Port: 4000")
+})
